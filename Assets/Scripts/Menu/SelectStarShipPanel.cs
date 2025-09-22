efbd78b1e2f81d323ebc172178dfc4bf855ab72f
gimminz <1105kmj@gmail.com>
2025-09-22 18:01:06 +0900

    fix: back Button

--- Assets/Scripts/Menu/SelectStarShipPanel.cs
+++ Assets/Scripts/Menu/SelectStarShipPanel.cs
@@ -4,4 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class SelectStartShipPanel : GenericWindow
 {
+    public Button backButton;
+    private void Awake()
+    {
+        if (backButton != null) backButton.onClick.AddListener(() => manager.Open(Menus.MainMenuSelect));
+    }
+
+    public override void Open()
+    {
+        if (backButton != null) firstSelected = backButton.gameObject;
+
+        base.Open();
+    }
 }
